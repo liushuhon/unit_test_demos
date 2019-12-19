6814b445b9adecb0315b55f9a8bd1cb920de2eea
liuliu <815835778@qq.com>
2019-12-19 11:48:34 +0800

    feat(测试):测试用例更改

--- src/test/password.spec.js
+++ src/test/password.spec.js
@@ -10,10 +10,10 @@ describe('密码(宽松)：密码位数6-20位', function() {
     });
 
     it('6位应该是true', function() {
-        expect(reg.test('segma1')).to.be.false;
+        expect(reg.test('segma1')).to.be.true;
     });
 
     it('20位应该是true', function() {
-        expect(reg.test('segmasegmasegmasegma')).to.be.false;
+        expect(reg.test('segmasegmasegmasegma')).to.be.true;
     });
 });

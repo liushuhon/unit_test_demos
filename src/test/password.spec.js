df316daa2b277e402033199e6406f16d5e271b65
liuliu <815835778@qq.com>
2019-12-19 11:45:22 +0800

    feat(测试):密码正则验证

---
+++ src/test/password.spec.js
@@ -0,0 +1,19 @@
+let expect = require('chai').expect;
+const reg = /^.{6,20}$/;
+describe('密码(宽松)：密码位数6-20位', function() {
+    it('五位应该是false', function() {
+        expect(reg.test('segma')).to.be.false;
+    });
+
+    it('21位应该是false', function() {
+        expect(reg.test('segmasegmasegmasegma1')).to.be.false;
+    });
+
+    it('6位应该是true', function() {
+        expect(reg.test('segma1')).to.be.false;
+    });
+
+    it('20位应该是true', function() {
+        expect(reg.test('segmasegmasegmasegma')).to.be.false;
+    });
+});

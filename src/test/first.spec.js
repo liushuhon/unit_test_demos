5a760ff5a8dfa02b876d3583de4d2fcfe5187eac
liuliu <815835778@qq.com>
2019-12-19 11:10:59 +0800

    feat(测试):增加用例

--- src/test/first.spec.js
+++ src/test/first.spec.js
@@ -4,4 +4,8 @@ describe('my first describe', function() {
     it('true is true', function() {
         expect(true).to.be.true;
     });
+
+    it('false is false',function() {
+        expect(false).to.be.true;
+    })
 })

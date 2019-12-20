3f8da63d1287e1ef06929c8154a84f8bd56589ea
liuliu <815835778@qq.com>
2019-12-20 16:55:31 +0800

    feat(配置): 各种配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+
+let plugin = [];
+if(process.env["ENV"] === "test") {
+    plugin.push('istanbul');
+
+}
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+
+    plugins: ['istanbul']
 };
--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,17 @@ module.exports = function(config) {
             ]
         },
         webpack: webpackConfig,
-        reporters: ['spec'],
+        reporters: ['spec','coverage'],
+        coverageReporter: {
+            // 配置生成的代码覆盖率文件存放位置
+            dir: './coverage',
+            reporters: [
+                // 生成 lcov.info 以及 html 文件，lcov.info 该文件中包含了详细的每个文件，每行，每个函数的执行信息。
+                { type: 'lcov', subdir: '.' },
+                // 在命令行输出简要覆盖率数据
+                { type: 'text-summary' }
+            ]
+        },
         browsers: ['PhantomJS'],
         singleRun: true
     })
